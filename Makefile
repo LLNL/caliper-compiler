eeafa5b7840c0152b93613f2a1b90eef838e94cd
David Zoeller Poliakoff <david.poliakoff@gmail.com>
2017-05-31 09:28:08 -0700

    Initial commit for Github

---
+++ Makefile
@@ -0,0 +1,162 @@
+# Copyright (c) 2015, Lawrence Livermore National Security, LLC.  
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Caliper.
+# Written by David Boehme, boehme3@llnl.gov.
+# LLNL-CODE-678900
+# All rights reserved.
+#
+# For details, see https://github.com/scalability-llnl/Caliper.
+# Please also see the LICENSE file for our additional BSD notice.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this list of
+#    conditions and the disclaimer below.
+#  * Redistributions in binary form must reproduce the above copyright notice, this list of
+#    conditions and the disclaimer (as noted below) in the documentation and/or other materials
+#    provided with the distribution.
+#  * Neither the name of the LLNS/LLNL nor the names of its contributors may be used to endorse
+#    or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+# LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#-------------------------------------------------------------------------------
+# Like all Clang plugin projects on Github, this one is inspired by Eli 
+# Bendersky's (eliben@gmail.com) blog and sample repository
+#
+# Sample makefile for building the code samples. Read inline comments for
+# documentation.
+#
+# Eli Bendersky (eliben@gmail.com)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+
+# The following variables will likely need to be customized, depending on where
+# and how you built LLVM & Clang. They can be overridden by setting them on the
+# make command line: "make VARNAME=VALUE", etc.
+
+# LLVM_SRC_PATH is the path to the root of the checked out source code. This
+# directory should contain the configure script, the include/ and lib/
+# directories of LLVM, Clang in tools/clang/, etc.
+#
+# Alternatively, if you're building vs. a binary distribution of LLVM
+# (downloaded from llvm.org), then LLVM_SRC_PATH can point to the main untarred
+# directory of the binary download (the directory that has bin/, lib/, include/
+# and other directories inside).
+# See the build_vs_released_binary.sh script for an example.
+
+
+CLANG_LOC_GUESS = $(shell which clang)
+LLVM_SRC_PATH := $(shell echo $(CLANG_LOC_GUESS) | rev | sed "s,.*/nib/,/," | rev)
+
+# LLVM_BUILD_PATH is the directory in which you built LLVM - where you ran
+# configure or cmake.
+# For linking vs. a binary build of LLVM, point to the main untarred directory.
+# LLVM_BIN_PATH is the directory where binaries are placed by the LLVM build
+# process. It should contain the tools like opt, llc and clang. The default
+# reflects a release build with CMake and Ninja. binary build of LLVM, point it
+# to the bin/ directory.
+LLVM_BUILD_PATH := $(LLVM_SRC_PATH)
+LLVM_BIN_PATH 	:= $(LLVM_BUILD_PATH)/bin/
+
+$(info -----------------------------------------------)
+$(info Using LLVM_SRC_PATH = $(LLVM_SRC_PATH))
+$(info Using LLVM_BUILD_PATH = $(LLVM_BUILD_PATH))
+$(info Using LLVM_BIN_PATH = $(LLVM_BIN_PATH))
+$(info -----------------------------------------------)
+
+# CXX has to be a fairly modern C++ compiler that supports C++11. gcc 4.8 and
+# higher or Clang 3.2 and higher are recommended. Best of all, if you build LLVM
+# from sources, use the same compiler you built LLVM with.
+# Note: starting with release 3.7, llvm-config will inject flags that gcc may
+# not support (for example '-Wcovered-switch-default'). If you run into this
+# problem, build with CXX set to a modern clang++ binary instead of g++.
+CXX := clang++
+CXXFLAGS := -fno-rtti -O0 -g -std=c++1z -w
+PLUGIN_CXXFLAGS := -fpic
+
+LLVM_CXXFLAGS := `$(LLVM_BIN_PATH)/llvm-config --cxxflags` -std=c++1z -w -g
+LLVM_LDFLAGS := `$(LLVM_BIN_PATH)/llvm-config --ldflags --libs --system-libs`
+
+# Plugins shouldn't link LLVM and Clang libs statically, because they are
+# already linked into the main executable (opt or clang). LLVM doesn't like its
+# libs to be linked more than once because it uses globals for configuration
+# and plugin registration, and these trample over each other.
+LLVM_LDFLAGS_NOLIBS := `$(LLVM_BIN_PATH)/llvm-config --ldflags` -std=c++1z -w -g
+PLUGIN_LDFLAGS := -shared
+
+# These are required when compiling vs. a source distribution of Clang. For
+# binary distributions llvm-config --cxxflags gives the right path.
+CLANG_INCLUDES := \
+	-I$(LLVM_SRC_PATH)/tools/clang/include \
+	-I$(LLVM_BUILD_PATH)/tools/clang/include
+
+# List of Clang libraries to link. The proper -L will be provided by the
+# call to llvm-config
+# Note that I'm using -Wl,--{start|end}-group around the Clang libs; this is
+# because there are circular dependencies that make the correct order difficult
+# to specify and maintain. The linker group options make the linking somewhat
+# slower, but IMHO they're still perfectly fine for tools that link with Clang.
+CLANG_LIBS := \
+	-Wl,--start-group \
+	-lclangAST \
+	-lclangASTMatchers \
+	-lclangIndex \
+	-lclangAnalysis \
+	-lclangBasic \
+	-lclangDriver \
+	-lclangEdit \
+	-lclangFrontend \
+	-lclangFrontendTool \
+	-lclangLex \
+	-lclangParse \
+	-lclangSema \
+	-lclangEdit \
+	-lclangRewrite \
+	-lclangRewriteFrontend \
+	-lclangStaticAnalyzerFrontend \
+	-lclangStaticAnalyzerCheckers \
+	-lclangStaticAnalyzerCore \
+	-lclangSerialization \
+	-lclangToolingCore \
+	-lclangTooling \
+	-lclangFormat \
+	-Wl,--end-group
+
+# Internal paths in this project: where to find sources, and where to put
+# build artifacts.
+SRC_LLVM_DIR := src_llvm
+SRC_CLANG_DIR := src_clang
+BUILDDIR := build
+
+.PHONY: all
+default: cali
+
+cali: make_builddir \
+	$(BUILDDIR)/caliper_instrumenter
+
+.PHONY: emit_build_config
+emit_build_config: make_builddir
+	@echo $(LLVM_BIN_PATH) > $(BUILDDIR)/_build_config
+
+.PHONY: make_builddir
+make_builddir:
+	@test -d $(BUILDDIR) || mkdir $(BUILDDIR)
+
+$(BUILDDIR)/caliper_instrumenter: $(SRC_CLANG_DIR)/caliper_instrumenter.cpp
+	$(CXX) $(CXXFLAGS) $(LLVM_CXXFLAGS) $(CLANG_INCLUDES) $^ \
+		$(CLANG_LIBS) $(LLVM_LDFLAGS) -o $@
+
+.PHONY: clean
+clean:
+	rm -rf $(BUILDDIR)/* *.dot test/*.pyc test/__pycache__
---
+++ src_clang/caliper_instrumenter.cpp
@@ -0,0 +1,256 @@
+// Copyright (c) 2015, Lawrence Livermore National Security, LLC.  
+// Produced at the Lawrence Livermore National Laboratory.
+//
+// This file is part of Caliper.
+// Written by David Boehme, boehme3@llnl.gov.
+// LLNL-CODE-678900
+// All rights reserved.
+//
+// For details, see https://github.com/scalability-llnl/Caliper.
+// Please also see the LICENSE file for our additional BSD notice.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the disclaimer below.
+//  * Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the disclaimer (as noted below) in the documentation and/or other materials
+//    provided with the distribution.
+//  * Neither the name of the LLNS/LLNL nor the names of its contributors may be used to endorse
+//    or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+//------------------------------------------------------------------------------
+// Clang tool which instruments a file for use with Caliper
+//
+// David Poliakoff (poliakoff1@llnl.gov)
+//------------------------------------------------------------------------------
+
+#include <cstdio>
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Parse/ParseAST.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Rewrite/Frontend/Rewriters.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <regex>
+#include <set>
+#include <fstream>
+#include <iterator>
+#include "infrastructure/llnl_static_analysis_utilities.h"
+
+using namespace clang;
+
+CompilerInstance TheCompInst;
+std::regex m_regex;
+std::vector<std::regex> whitelisted_funcs;
+std::vector<std::regex> blacklisted_funcs;
+static bool rewrit = false;
+// By implementing RecursiveASTVisitor, we can specify which AST nodes
+// we're interested in by overriding relevant methods.
+class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
+public:
+  MyASTVisitor(Rewriter &R) : TheRewriter(R) {}
+  std::string getCaliStartAnnotation(std::string name){
+    return std::string("if (cali_function_attr_id == CALI_INV_ID)\n " \
+            " cali_init();\n " \
+             " cali_begin_string(cali_function_attr_id, \"") + name + std::string("\");\n");
+  }
+  template <typename handled_type, typename modifier>
+  void modify_recursively(const Stmt *modified, modifier mod)
+  {
+    operateOnType<handled_type>(modified,
+                                [&](const handled_type *stm) { mod(stm); });
+    if (modified) {
+      operateOnType<Stmt>(modified, [&](const Stmt *checked_modified) {
+        std::vector<const Stmt *> rev_vec;
+        for (auto *child : checked_modified->children()) {
+          if (child) {
+            rev_vec.push_back(child);
+          }
+        };
+        for (auto *child : rev_vec) {
+          modify_recursively<ReturnStmt>(child, mod);
+        }
+      });
+    }
+  }
+ 
+  bool VisitFunctionDecl(FunctionDecl *f) {
+    // Only function definitions (with bodies), not declarations.
+     static FileID moddedFile;
+     bool hits = false;
+     std::string fname = f->getQualifiedNameAsString();
+     for(auto reg : whitelisted_funcs){
+       hits |= std::regex_search(fname, reg);
+     }
+     if (TheRewriter.getSourceMgr().getFileID(f->getLocStart()) != moddedFile) {
+       moddedFile = TheRewriter.getSourceMgr().getFileID(f->getLocStart());
+       SourceLocation fileStartLoc = TheRewriter.getSourceMgr().translateLineCol(moddedFile,1,1);
+       TheRewriter.InsertTextBefore(fileStartLoc, "#include<caliper/cali.h>\n");
+     }
+     hits = true;
+     if (hits) {
+        if (f->hasBody()) {
+          rewrit = true;
+          Stmt *FuncBody = f->getBody();
+
+          modify_recursively<ReturnStmt>(FuncBody, [&]( const ReturnStmt* ret){
+            SourceLocation locStart = ret->getLocStart();
+            TheRewriter.InsertTextBefore(locStart," {cali_end(cali_function_attr_id);");
+            SourceLocation locEnd = ret->getLocEnd();
+            if(ret->getRetValue()){
+              int tokLength = Lexer::MeasureTokenLength(ret->getRetValue()->getLocEnd(),TheRewriter.getSourceMgr(),TheCompInst.getLangOpts());
+              locEnd=ret->getRetValue()->getLocEnd().getLocWithOffset(tokLength+1);
+              TheRewriter.InsertTextAfter(locEnd,"}\n");
+            }
+            else{
+              //locEnd = locEnd.getLocWithOffset(8);
+              TheRewriter.InsertTextAfter(locEnd,"}\n");
+            }
+          });
+          auto firstStatementChildVec = FuncBody->children();
+          if(firstStatementChildVec.begin()!=firstStatementChildVec.end()){
+            auto firstStatement = firstStatementChildVec.begin();
+            TheRewriter.InsertTextBefore(firstStatement->getSourceRange().getBegin(),getCaliStartAnnotation(f->getNameAsString()));
+            TheRewriter.InsertTextBefore(f->getBodyRBrace(),"cali_end(cali_function_attr_id);\n");
+          }
+          //// Type name as string
+          //QualType QT = f->getReturnType();
+          //std::string TypeStr = QT.getAsString();
+
+          //// Function name
+          //DeclarationName DeclName = f->getNameInfo().getName();
+          //std::string FuncName = DeclName.getAsString();
+
+          //// Add comment before
+          //std::stringstream SSBefore;
+          //SSBefore << "// Begin function " << FuncName << " returning " << TypeStr
+          //         << "\n";
+          //SourceLocation ST = f->getSourceRange().getBegin();
+          //TheRewriter.InsertText(ST, SSBefore.str(), true, true);
+
+          //// And after
+          //std::stringstream SSAfter;
+          //SSAfter << "\n// End function " << FuncName;
+          //ST = FuncBody->getLocEnd().getLocWithOffset(1);
+          //TheRewriter.InsertText(ST, SSAfter.str(), true, true);
+        }
+
+     }
+    return true;
+  }
+
+private:
+  Rewriter &TheRewriter;
+};
+
+std::set<FileID> containers;
+
+// Implementation of the ASTConsumer interface for reading an AST produced
+// by the Clang parser.
+class MyASTConsumer : public ASTConsumer {
+public:
+  MyASTConsumer(Rewriter &R) : Visitor(R), TheRewriter(R) {}
+
+  // Override the method that gets called for each parsed top-level
+  // declaration.
+  virtual bool HandleTopLevelDecl(DeclGroupRef DR) {
+    if(DR.begin() != DR.end()){
+      auto first_decl = *(DR.begin());
+      //TheRewriter.InsertTextBefore(first_decl->getLocStart(), "#include<caliper/cali.h>\n");
+    }
+    for (DeclGroupRef::iterator b = DR.begin(), e = DR.end(); b != e; ++b)
+      // Traverse the declaration using our AST visitor.
+      Visitor.TraverseDecl(*b);
+    return true;
+  }
+
+private:
+  MyASTVisitor Visitor;
+  Rewriter &TheRewriter;
+};
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    llvm::errs() << "Usage: rewritersample <filename> <regexes>\n";
+    return 1;
+  }
+
+  for(int whitelisted = 2;whitelisted<argc;whitelisted++){
+    whitelisted_funcs.emplace_back(std::regex(argv[whitelisted],std::regex::optimize));
+  }
+  //whitelisted_funcs.emplace_back(std::regex(".*",std::regex::optimize));
+  m_regex = std::regex(".*", std::regex::optimize);
+  // CompilerInstance will hold the instance of the Clang compiler for us,
+  // managing the various objects needed to run the compiler.
+  TheCompInst.createDiagnostics();
+
+  LangOptions &lo = TheCompInst.getLangOpts();
+  lo.CPlusPlus = 1;
+
+  // Initialize target info with the default triple for our platform.
+  auto TO = std::make_shared<TargetOptions>();
+  TO->Triple = llvm::sys::getDefaultTargetTriple();
+  TargetInfo *TI =
+      TargetInfo::CreateTargetInfo(TheCompInst.getDiagnostics(), TO);
+  TheCompInst.setTarget(TI);
+
+  TheCompInst.createFileManager();
+  FileManager &FileMgr = TheCompInst.getFileManager();
+  TheCompInst.createSourceManager(FileMgr);
+  SourceManager &SourceMgr = TheCompInst.getSourceManager();
+  TheCompInst.createPreprocessor(TU_Module);
+  TheCompInst.createASTContext();
+
+  // A Rewriter helps us manage the code rewriting task.
+  Rewriter TheRewriter;
+  TheRewriter.setSourceMgr(SourceMgr, TheCompInst.getLangOpts());
+
+  // Set the main file handled by the source manager to the input file.
+  const FileEntry *FileIn = FileMgr.getFile(argv[1]);
+  SourceMgr.setMainFileID(
+      SourceMgr.createFileID(FileIn, SourceLocation(), SrcMgr::C_User));
+  TheCompInst.getDiagnosticClient().BeginSourceFile(
+      TheCompInst.getLangOpts(), &TheCompInst.getPreprocessor());
+
+  // Create an AST consumer instance which is going to get called by
+  // ParseAST.
+  MyASTConsumer TheConsumer(TheRewriter);
+
+  // Parse the file to AST, registering our consumer as the AST consumer.
+  ParseAST(TheCompInst.getPreprocessor(), &TheConsumer,
+           TheCompInst.getASTContext());
+
+  // At this point the rewriter's buffer should be full with the rewritten
+  // file contents.
+  const RewriteBuffer *RewriteBuf =
+      TheRewriter.getRewriteBufferFor(SourceMgr.getMainFileID());
+  if(true){
+    llvm::outs() << std::string(RewriteBuf->begin(), RewriteBuf->end());
+  }
+  return 0;
+}
